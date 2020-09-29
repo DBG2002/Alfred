f1534a934c11ccf5edc9274cb725c382cd1c4134
Caio Piccirillo <caio.piccirillo@tecsidel.com>
2020-09-29 20:49:13 -0300

    Changes: 1. New directories 2. Main script 3. Separation of reference scripts into classes 4. Venv deleted and replaced by requirements.txt

---
+++ scripts/main.py
@@ -0,0 +1,5 @@
+import face_tracker
+
+if __name__ == "__main__":
+    ft = face_tracker.FaceTracker()
+    ft.begin()
---
+++ setup.py
@@ -0,0 +1,7 @@
+''' If you need to setup your project use this script
+    Take a look at: https://docs.python.org/3/distutils/setupscript.html
+    For now, we'll use directly the scripts in "./scripts"
+
+    Execute "pip freeze > requirements.txt" in terminal to tell other people which
+    packages they'll use to run your project.
+'''
